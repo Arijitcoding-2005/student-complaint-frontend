75824ee7a34ae88fc2b3c67059ddb59ece1042c4
Arijit Saha <142727976+Arijitcoding-2005@users.noreply.github.com>
2026-03-10 11:07:09 +0530

    updated API base URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://campus-complaint-management-backend.onrender.com",
 });
 
 // Attach JWT token automatically
